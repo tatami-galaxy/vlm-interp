7fa370d7b3a66c8e3a7d60c7182a257c55b453bb
ujan <ujandev@gmail.com>
2024-12-02 22:40:33 +0530

    eval other models. interp with qwen

--- repo_setup.py
+++ repo_setup.py
@@ -5,11 +5,15 @@ import subprocess
 def main():
 
     req = ['transformers', 'datasets', 'accelerate', 'qwen_vl_utils']
-
     # install packages
     for package in req:
         subprocess.run(["pip", "install", package])
     subprocess.run(["pip", "install", "flash-attn", "--no-build-isolation"])
+
+    # rclone -> test
+    subprocess.run(["curl", "https://rclone.org/install.sh"])
+    subprocess.run(["bash"])
+    subprocess.run(["rclone", "config"])
 
 
 if __name__ == "__main__":
