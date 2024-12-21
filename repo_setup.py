8bad1fd9d6633c5c09f8fe3daffbff3865edde52
ujan <ujandev@gmail.com>
2024-12-21 20:54:42 +0530

    map clip embeddings to image patches

--- repo_setup.py
+++ repo_setup.py
@@ -1,19 +1,9 @@
 import os
-import subprocess
 
 
 def main():
 
-    req = ['transformers', 'datasets', 'accelerate', 'qwen_vl_utils']
-    # install packages
-    for package in req:
-        subprocess.run(["pip", "install", package])
-    subprocess.run(["pip", "install", "flash-attn", "--no-build-isolation"])
-
-    # rclone -> test
-    subprocess.run(["curl", "https://rclone.org/install.sh"])
-    subprocess.run(["bash"])
-    subprocess.run(["rclone", "config"])
+    pass
 
 
 if __name__ == "__main__":
