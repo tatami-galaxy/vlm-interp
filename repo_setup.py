251595fedd8854df4579d2cc2d7046683350c04c
Ujan <ujandev@gmail.com>
2024-12-23 14:28:42 +0530

    map clip embeddings to image patches

--- repo_setup.py
+++ repo_setup.py
@@ -1,9 +1,27 @@
 import os
+import subprocess
 
 
 def main():
 
-    pass
+    req = [
+        'matplotlib', "bitsandbytes",
+        'transformers', 'datasets', 'accelerate',
+        'evaluate', 'tensorboard', 'wandb',
+    ]
+
+
+    # directorries
+    os.mkdir('data')
+    os.mkdir('data/raw')
+    os.mkdir('data/processed')
+
+    os.mkdir('models')
+
+    # install packages
+    for package in req:
+        subprocess.run(["pip", "install", package])
+    subprocess.run(["flash-attn", "--no-build-isolation"])
 
 
 if __name__ == "__main__":
