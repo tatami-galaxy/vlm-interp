1873f1616bbdf0419d9d7cb4f435292f3adf3463
Ujan <ujandev@gmail.com>
2024-12-30 13:50:41 +0530

    logit len local vs perf. attention map. diffusion

---
+++ src/llava_logit_lens_loc_demo.py
@@ -0,0 +1,49 @@
+"""do logit lens object localization for an image"""
+
+from dataclasses import dataclass, field
+
+from PIL import Image
+import matplotlib.pyplot as plt
+from transformers import HfArgumentParser
+
+from utils import llava_load_model, llava_process_image, llava_forward_pass
+
+
+@dataclass
+class Arguments:
+    """arguments"""
+
+    dataset_path: str = field(
+        default='/home/drdo/vlm-compositionality/data/raw/sugarcrepe',
+    )
+    image_folder: str = field(
+        default='/home/drdo/vlm-compositionality/data/raw/coco_val_2017'
+    )
+    model_name: str = field(default="llava-hf/llava-1.5-13b-hf")
+    image_file: str = field(default=None)
+
+
+if __name__ == "__main__":
+
+    # parse cl arguments
+    parser = HfArgumentParser(Arguments)
+    args = parser.parse_args_into_dataclasses()[0]
+
+    # load image
+    # TODO: random image
+    assert args.image_file is not None
+    image = Image.open(args.image_folder+'/'+args.image_file)
+
+    # load model, processor
+    model, processor = llava_load_model(args.model_name)
+
+    # process image and prompt(default)
+    inputs = llava_process_image(image, processor, device=model.device)
+
+    # forward pass
+    _, hidden_states, _ = llava_forward_pass(inputs, model, output_hidden_states=True)
+    
+
+
+    plt.show()
+# End-of-file (EOF)
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,81 @@
 """basic functions"""
 
-from PIL import Image
+import json
+import torch
+from transformers import LlavaForConditionalGeneration, AutoProcessor
 
 
-def llava_process_image(image_file, processor, device, prompt='default'):
+def load_sugarcrepe(folder_path):
+    """load sugarcrepe dataset from local"""
+
+    # add attribute
+    with open (folder_path+'add_att.json', encoding='utf8') as f:
+        add_attribute = json.load(f)
+
+    # add object
+    with open (folder_path+'add_obj.json', encoding='utf8') as f:
+        add_object = json.load(f)
+
+    # replace attribute
+    with open (folder_path+'replace_att.json', encoding='utf8') as f:
+        replace_attribute = json.load(f)
+
+    # replace object
+    with open (folder_path+'replace_obj.json', encoding='utf8') as f:
+        replace_object = json.load(f)
+
+    # replace relation
+    with open (folder_path+'replace_rel.json', encoding='utf8') as f:
+        replace_relation = json.load(f)
+
+    # swap attribute
+    with open (folder_path+'swap_att.json', encoding='utf8') as f:
+        swap_attribute = json.load(f)
+
+    # swap object
+    with open (folder_path+'swap_obj.json', encoding='utf8') as f:
+        swap_object = json.load(f)
+
+    # collate together
+    dataset = {
+        'add_attribute': add_attribute, 'add_object': add_object, 
+        'replace_attribute': replace_attribute, 'replace_object': replace_object, 
+        'replace_relation': replace_relation, 'swap_attribute': swap_attribute, 
+        'swap_object': swap_object,
+    }
+
+    return dataset
+
+
+def llava_load_model(model_name, flash_attention=True):
+    """load llava model"""
+    if flash_attention:
+        model =  LlavaForConditionalGeneration.from_pretrained(
+            model_name,
+            torch_dtype=torch.bfloat16,
+            attn_implementation="flash_attention_2",
+            device_map="auto",
+        )
+    else:
+        model =  LlavaForConditionalGeneration.from_pretrained(
+            model_name,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+        )
+    processor = AutoProcessor.from_pretrained(model_name)
+    return model, processor
+
+
+def llava_process_image(image, processor, device, prompt='default'):
     """Process image and prompt into input for llava"""  
-    image = Image.open(image_file)
     if prompt == 'default':
         prompt = "USER: <image>\nDescribe the image. ASSISTANT:"
     inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
     return inputs
 
 
-def forward_pass(inputs, model, output_hidden_states=True, output_attentions=True):
+def llava_forward_pass(inputs, model, output_hidden_states=False, output_attentions=False):
     """Forward pass through the model to collect logits, hidden_states, attentions"""
-    assert input.device == model.device
     output = model(
         **inputs,
         output_hidden_states=output_hidden_states,
